7bc4c42e04b2c3005ae582175159c0b2c5b635f0
Stefano <stefano.funaro@gmail.com>
2021-10-07 15:09:17 +0200

    create repo

---
+++ index.php
@@ -0,0 +1,29 @@
+<!-- Creare una variabile con un paragrafo di testo a vostra scelta. -->
+<?php 
+    $lorem = '
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales porttitor feugiat. Aenean ac metus ut risus pretium rhoncus. Nulla et lobortis eros. Nam mattis, orci sit amet scelerisque eleifend, arcu dolor eleifend turpis, a laoreet est odio quis nunc. Aliquam erat volutpat. Donec at est vitae nunc malesuada volutpat. Cras lobortis a tortor non varius. Ut et dapibus nibh.'
+?>
+
+<h1>
+    <?php 
+        echo $lorem
+    ?>
+</h1>
+
+
+
+<!-- Stampare a schermo il paragrafo e la sua lunghezza. -->
+
+
+
+
+
+
+<!-- Una parola da censurare viene passata dall'utente tramite parametro GET. -->
+
+
+
+
+
+
+<!-- Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
